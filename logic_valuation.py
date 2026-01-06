c50efd3828ed6beb501590c1971dba2d30d5009e
favoritemaths-dotcom <favoritemaths@gmail.com>
2026-01-06 17:19:14 +0530

    Update

---
+++ logic_valuation.py
@@ -0,0 +1,29 @@
+import yfinance as yf
+
+def estimate_fair_value(symbol, fund, get_cmp):
+    try:
+        eps = yf.Ticker(symbol + ".NS").info.get("trailingEps")
+    except:
+        return None, None, None
+
+    pe = fund.get("PE")
+    if eps is None or pe is None:
+        return None, None, None
+
+    fair_pe = 18 if pe <= 15 else 22 if pe <= 25 else 25
+    fair_value = round(eps * fair_pe)
+
+    cmp = get_cmp(symbol)
+    if cmp is None:
+        return fair_value, None, None
+
+    upside = round((fair_value - cmp) / cmp * 100, 1)
+
+    if cmp <= 0.85 * fair_value:
+        zone = "🟢 Attractive"
+    elif cmp <= fair_value:
+        zone = "🟡 Reasonable"
+    else:
+        zone = "🔴 Expensive"
+
+    return fair_value, upside, zone
