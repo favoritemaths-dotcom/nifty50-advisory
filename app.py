143668e929d5f247c5f9ad8e7f7879634de382fd
favoritemaths-dotcom <favoritemaths@gmail.com>
2026-01-09 12:48:40 +0530

    Update

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from logic_news import analyze_news
 from logic_quarterly import analyze_quarterly_text
 from logic_scoring import score_stock, detect_profile_mismatch
 from logic_explanation import generate_explanation
-from logic_confidence import confidence_band, risk_triggers
+from logic_confidence import confidence_band, risk_triggers, conviction_label
 from logic_market_regime import detect_market_regime
 from logic_portfolio import analyze_portfolio
 
